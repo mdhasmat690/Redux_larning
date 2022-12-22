22859771681ccc527cd5ef4f390470595139d972
mdhasmat690 <mdhasmat690@gmail.com>
2022-12-22 14:57:46 +0600

    second branch created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import LongForm from "./pages/LongForm";
+import { RouterProvider } from "react-router-dom";
+import ProductProvider from "./context/ProductProvider";
+import routes from "./routes/routes";
 
 function App() {
-  return <LongForm />;
+  return (
+    <div>
+      <ProductProvider>
+        <RouterProvider router={routes} />
+      </ProductProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/in/index.js
+++
@@ -1,8 +0,0 @@
-let player = [{name: "sakib"},{name: "maruf"}]
-
-console.log(player);
-
-player = [...player,{name: "Himu"}]
-console.log(player);
-console.log(player);
-console.log(player);
