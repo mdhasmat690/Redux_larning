b7f10d40e89fcc0e4539fcd0e8c9e44cdda8de14
mdhasmat690 <mdhasmat690@gmail.com>
2022-12-23 22:41:52 +0600

    initial set up complate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import ProductCard from "../components/ProductCard";
-import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const {
-    state: { products, loading, error },
-  } = useProducts();
+  const [products, setProduct] = useState([]);
+  useEffect(() => {
+    fetch("/service.json")
+      .then((res) => res.json())
+      .then((data) => setProduct(data));
+  }, []);
 
-  let content;
-
-  if (loading) {
-    content = <p>Loading</p>;
-  }
-
-  if (error) {
-    content = <p>Something went wrong</p>;
-  }
-
-  if (!loading && !error && products.length === 0) {
-    content = <p>Nothing to show, product list is empty</p>;
-  }
-
-  if (!loading && !error && products.length) {
-    content = products.map((product) => (
-      <ProductCard key={product._id} product={product} />
-    ));
-  }
-
+  const state = useSelector((state) => state);
+  console.log(state);
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {content}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
     </div>
   );
 };
